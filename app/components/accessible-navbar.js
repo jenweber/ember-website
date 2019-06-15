7aec39d90873bc168abf0883d3b7be10cb3663c8
Jen Weber <j@jenweber.me>
2019-06-15 18:30:11 -0400

    create dropdown functionality

--- app/components/accessible-navbar.js
+++ app/components/accessible-navbar.js
@@ -1,4 +1,23 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    docs: false,
+    releases: false,
+    community: false,
+    about: false,
+
+    actions: {
+        show(item) {
+            let current = this.get(item)
+            this.resetToggles()
+            this.set(item, !current)
+        }
+    },
+
+    resetToggles() {
+        this.set('docs', false)
+        this.set('releases', false)
+        this.set('community', false)
+        this.set('about', false)
+    }
 });

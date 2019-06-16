d127bc252417a2241921515643b4d691c8dad46f
Jen Weber <j@jenweber.me>
2019-06-15 23:07:58 -0400

    handle release versions better

--- app/routes/releases.js
+++ app/routes/releases.js
@@ -1,12 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model() {
-    return {
-        latest: '3.10',
-				activeLTS: ['2.18', '3.4', '3.8'],
-        beta: '',
-        canary: ''
-    }
-  }
 });

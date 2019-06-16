486370e95a510f76866ab3d123e4956af8fdbe81
Jen Weber <j@jenweber.me>
2019-06-15 21:24:29 -0400

    add links to release routes and close nav on transition

--- app/components/accessible-navbar.js
+++ app/components/accessible-navbar.js
@@ -1,6 +1,9 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { observer } from '@ember/object'
 
 export default Component.extend({
+    router: service(),
     docs: false,
     releases: false,
     community: false,
@@ -19,5 +22,16 @@ export default Component.extend({
         this.set('releases', false)
         this.set('community', false)
         this.set('about', false)
+    },
+
+    onTransition: observer('router.currentRouteName', function() {
+        // TODO refactor to not use an observer
+        this.resetToggles()
+    }),
+
+    init() {
+        // initialize the router service
+        this.get('router.currentRouteName')
+        return this._super(...arguments)
     }
 });
